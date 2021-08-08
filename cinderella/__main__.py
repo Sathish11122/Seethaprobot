a357c54a6460e070285e8bffe6970129694f1d01
Sathish11122 <88620962+Sathish11122@users.noreply.github.com>
2021-08-08 19:15:42 +0530

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -182,9 +182,9 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="Help🤗",callback_data="help_back"),InlineKeyboardButton(text="Founder👿",url="https://t.me/Naanevaayadi_mass")]]
+    keyboard = [[InlineKeyboardButton(text="Help🤗",callback_data="help_back"),InlineKeyboardButton(text="Founder👿",url="https://t.me/sthish420_paiyan")]]
     keyboard += [[InlineKeyboardButton(text="Connect Group😇", callback_data="main_connect"),InlineKeyboardButton(text="Add Me😌",url="t.me/{}?startgroup=true".format(bot.username))]]
-    keyboard += [[InlineKeyboardButton(text="Team_VO🥰",url="https://t.me/Team_vo_proofs"),InlineKeyboardButton(text="💞Anbe aaruyire💞",url="https://t.me/anbe_aaruyire")]]
+    keyboard += [[InlineKeyboardButton(text="Youtube🥰",url=" https://youtube.com/channel/UCz9l3DgBXC32rlPZtVzpNiw),InlineKeyboardButton(text="Insta😍",url="https://instagram.com/mr_sathish_007_?utm_medium=copy_link")]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
